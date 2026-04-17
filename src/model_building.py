dc572482ebdcdd69107d54d5036511594dd31060
Ashok <reddyashok075@gmail.com>
2026-04-17 12:55:27 +0530

    Params configured for the pipelinr

--- src/model_building.py
+++ src/model_building.py
@@ -28,6 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def lod_data(file_path:str)->pd.DataFrame:
     """
@@ -99,8 +115,10 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators':25,"random_state":2}
+        params = load_params('params.yaml')['model_building']
+
         tarin_data = lod_data('./data/processed/train_tfidf.csv')
+        
         X_train = tarin_data.iloc[:,:-1].values
         y_train = tarin_data.iloc[:,-1].values
 
